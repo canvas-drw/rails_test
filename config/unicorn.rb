e41aacd849259ca580cb6cb329aa5818acd1fa94
Devon Winders <dwinders@canvas.is>
2014-04-16 18:25:56 -0400

    update path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,16 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/www/rails_test"
+working_directory "/home/vagrant/www/rails_test"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/www/rails_test/pids/unicorn.pid"
+pid "/home/vagrant/www/rails_test/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/www/rails_test/log/unicorn.log"
-stdout_path "/www/rails_test/log/unicorn.log"
+stderr_path "/home/vagrant/www/rails_test/log/unicorn.log"
+stdout_path "/home/vagrant/www/rails_test/log/unicorn.log"
 
 # Unicorn socket
 listen "/tmp/unicorn.[app name].sock"
